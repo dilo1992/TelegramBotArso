be3800b74dea01c925dee99b5f8493d9a9359ad5
dmitrylobov <dilo1992.edu@gmail.com>
2023-03-20 22:24:48 +0300

    init commit

---
+++ src/main/java/by/dilo1992/telegrambotarso/TelegramBotArsoApplication.java
@@ -0,0 +1,13 @@
+package by.dilo1992.telegrambotarso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotArsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotArsoApplication.class, args);
+    }
+
+}
